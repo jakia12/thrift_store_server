40e0336393e4b5b05faff1357fbe155673722bf0
jakia12 <owasi110@gmail.com>
2022-11-26 01:13:40 +0600

    user delete route added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 //require mongo db
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //require dotenv
 require('dotenv').config();
@@ -65,28 +65,42 @@ async function run() {
         });
 
         app.get('/users', async (req, res) => {
-            const query = {};
+            const userType = req.query.userType;
+            let query = {};
+
+            if (userType) {
+                query = {
+                    userType: userType
+                }
+            }
             const users = await userCollection.find(query).toArray();
             res.send(users);
         });
 
-        //get the seller data
-        app.get('/users/:userType', async (req, res) => {
-            const userType = req.params.userType;
-
-            // let query = {};
-
-            // if (userType) {
-            //     query = {
-            //         userType: userType
-            //     }
-            //
-
-            const query = { userType };
-            const user = await userCollection.findOne(query);
+        //delete the users
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const user = await userCollection.deleteOne(query);
             res.send(user);
+        })
+        //get the seller data
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
 
+            const query = { email };
+            const user = await userCollection.findOne(query);
+            res.send({ isSeller: user?.userType === 'seller' });
 
+        })
+
+        //get the buyer data
+        app.get('/users/buyer/:email', async (req, res) => {
+            const email = req.params.email;
+
+            const query = { email };
+            const user = await userCollection.findOne(query);
+            res.send({ isBuyer: user?.userType === 'buyer' });
 
         })
     }
